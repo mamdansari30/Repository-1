2e450ba48ab9c0d91c05adbaa6ab0b35c0dcf784
HarshitaJethani <hj7162@srmist.edu.in>
2020-09-17 17:35:34 +0000

    Added first commit

---
+++ Sum/src/Sum.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : Sum.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, sum;
+	cin>>a>>b;
+	sum = a+b;
+	cout<<"Sum = "<<sum;
+	return 0;
+}
