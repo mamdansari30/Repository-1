86177ec6906db13cf3fe6dd79e89924935aafa48
HarshitaJethani <hj7162@srmist.edu.in>
2020-09-18 08:23:29 +0000

    Git Push

---
+++ GitPush/New.cpp
@@ -0,0 +1,19 @@
+/*
+ * New.cpp
+ *
+ *  Created on: Sep 18, 2020
+ *      Author: root
+ */
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
